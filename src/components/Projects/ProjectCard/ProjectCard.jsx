3aad1b9ba94a882bf4cc262d5c84d407b8543c9b
Lucas Gerhardt de Camargo <lucasgcamargo@icloud.com>
2021-02-12 11:56:28 -0800

    Add info about projects

--- src/components/Projects/ProjectCard/ProjectCard.jsx
+++ src/components/Projects/ProjectCard/ProjectCard.jsx
@@ -10,8 +10,8 @@ const ProjectCard = (props) => {
             <div className="card">
                 <div className="content">
                     <h2 className="title">Combatly (In Development)</h2>
-                    <p className="copy">Small war board game which you have to conquer the whole map <br/>
-                        Technologies: HTML, CSS, JAVASCRIPT
+                    <p className="copy">Small war board game which you have to conquer the whole map <br />
+                        Technologies: React, Firebase
                     </p>
                     <Button link="https://camarguin.github.io/Combatly-Web/" target="_blank">Visit Website</Button>
                 </div>
@@ -19,14 +19,18 @@ const ProjectCard = (props) => {
             <div className="card">
                 <div className="content">
                     <h2 className="title">Pro Caliber Lacross</h2>
-                    <p className="copy">Website made for a professional junior lacross club</p>
+                    <p className="copy">Website made for a professional junior lacross club<br />
+                        Technologies: HTML, CSS, JAVASCRIPT
+                        </p>
                     <Button link="https://camarguin.github.io/lacrosseProject/" target="_blank">Visit Website</Button>
                 </div>
             </div>
             <div className="card">
                 <div className="content">
                     <h2 className="title">Cricket Club</h2>
-                    <p className="copy">Website made for the West Vancouver cricket club</p>
+                    <p className="copy">Website made for the West Vancouver cricket club<br />
+                        Technologies: HTML, CSS, JAVASCRIPT
+                        </p>
                     <Button link="https://camarguin.github.io/Mid-Term-Project/" target="_blank">Visit Website</Button>
                 </div>
             </div>
